232c626615017287d46028f8fc6710103d98e3fd
Logan Snow <4197432+lsnow99@users.noreply.github.com>
2025-10-14 22:53:27 -0400

    cleanups and create non-comptime expand

--- src/root.zig
+++ src/root.zig
@@ -20,9 +20,9 @@ pub fn full() !void {
     
 }
 
-pub fn countDots(comptime name: []const u8) comptime_int {
-    comptime var dots = 0;
-    inline for (name) |char| {
+pub fn countDots(name: []const u8) comptime_int {
+    var dots = 0;
+    for (name) |char| {
         switch (char) {
             '.' => dots += 1,
             else => {},
@@ -31,13 +31,13 @@ pub fn countDots(comptime name: []const u8) comptime_int {
     return dots;
 }
 
-pub fn expand(comptime name: []const u8) [countDots(name) + 1][]const u8 {
+pub fn expandComptime(comptime name: []const u8) [countDots(name) + 1][]const u8 {
     comptime var result: [countDots(name) + 1][]const u8 = undefined;
 
     comptime var result_index = 0;
     comptime var i = 0;
 
-    inline while (true) {
+    inline while (i < name.len) : (i += 1){
         const start_index = i;
         inline while (i < name.len) : (i += 1) {
             switch (name[i]) {
@@ -49,43 +49,65 @@ pub fn expand(comptime name: []const u8) [countDots(name) + 1][]const u8 {
 
         result[result_index] = name[start_index..end_index];
         result_index += 1;
-        i += 1;
-
-        if (i >= name.len) {
-            break;
-        }
     }
 
     return result;
 }
 
-pub fn match(comptime names: []const []const u8, name: []const u8) []type {
-    comptime var i = 0;
-    inline while (true) {
-        const start_index = i;
-        inline while (i < name.len) : (i += 1) {
-            switch (name[i]) {
-                '.' => break,
-                else => {},
-            }
-        }
-        const end_index = i;
-        const name_slice = name[start_index..end_index];
+pub fn expand(allocator: std.mem.Allocator, name: []const u8) ![][]const u8 {
+    var result: std.ArrayList([]const u8) = .{};
+    defer result.deinit(allocator);
+    var parts = std.mem.splitScalar(u8, name, '.');
+    while (parts.next()) |part| {
+        try result.append(allocator, part);
     }
+    return result.toOwnedSlice(allocator);
+}
+
+// pub fn match(comptime names: []const []const u8, name: []const u8) []type {
+//     comptime var i = 0;
+//     inline while (true) {
+//         const start_index = i;
+//         inline while (i < name.len) : (i += 1) {
+//             switch (name[i]) {
+//                 '.' => break,
+//                 else => {},
+//             }
+//         }
+//         const end_index = i;
+//         const name_slice = name[start_index..end_index];
+//     }
+// }
+
+test "basic expandComptime" {
+    try std.testing.expect(expandComptime("foo.bar.baz").len == 3);
+    try std.testing.expect(std.mem.eql(u8, expandComptime("foo.bar.baz")[0], "foo"));
+    try std.testing.expect(std.mem.eql(u8, expandComptime("foo.bar.baz")[1], "bar"));
+    try std.testing.expect(std.mem.eql(u8, expandComptime("foo.bar.baz")[2], "baz"));
 }
 
 test "basic expand" {
-    try std.testing.expect(expand("foo.bar.baz").len == 3);
-    try std.testing.expect(std.mem.eql(u8, expand("foo.bar.baz")[0], "foo"));
-    try std.testing.expect(std.mem.eql(u8, expand("foo.bar.baz")[1], "bar"));
-    try std.testing.expect(std.mem.eql(u8, expand("foo.bar.baz")[2], "baz"));
+    const allocator = std.testing.allocator;
+
+    const expanded = (try expand(allocator, "foo.bar.baz"));
+    defer allocator.free(expanded);
+
+    try std.testing.expect(expanded.len == 3);
+
+    const foo = expanded[0];
+    const bar = expanded[1];
+    const baz = expanded[2];
+
+    try std.testing.expect(std.mem.eql(u8, foo, "foo"));
+    try std.testing.expect(std.mem.eql(u8, bar, "bar"));
+    try std.testing.expect(std.mem.eql(u8, baz, "baz"));
 }
 
-test "basic match" {
-    const names = .{
-        "foo.bar.baz",
-        "foo",
-    };
-
-    try std.testing.expect(match(names, "foo.bar.baz").len == 2);
-}
+// test "basic match" {
+//     const names = .{
+//         "foo.bar.baz",
+//         "foo",
+//     };
+//
+//     try std.testing.expect(match(names, "foo.bar.baz").len == 2);
+// }
